2a4b47826e4a231fe342c8b12964e4115adb0a53
David Acosta <dacostacsit@gmail.com>
2025-10-20 12:34:50 -0400

    Csharp records

---
+++ src/DotnetAndCSharp/Monkey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetAndCSharp.Zoo;
+public record Monkey
+{
+    public required string? Name { get; init; }
+    public int Age { get; init; }
+}
+
+public record Gorilla(string Name, int Age);
---
+++ src/DotnetAndCSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿global using DotnetAndCSharp.Zoo;
+
+Console.WriteLine("Hello, World!");
+
+var monkey1 = new Monkey()
+{
+    Name = "George"
+};
+Console.WriteLine(monkey1);
+
+var monkey2 = new Monkey()
+{
+    Name = "George",
+    Age = 3
+};
+Console.WriteLine(monkey2);
+
+var kong = new Gorilla("King", 32);
+Console.WriteLine(kong);
+var updatedKong = kong with { Name = "King Kong" };
+Console.WriteLine(updatedKong);
+
+if (monkey1.Name is not null)
+{
+    Console.WriteLine($"{monkey1.Name.ToUpper()}");
+}
+else 
+{
+    Console.WriteLine("Monkey has no name");
+}
+
+if (monkey1 == monkey2)
+{
+    Console.WriteLine("They are equivalent");
+}
+else 
+{
+    Console.WriteLine("Not the same Monkey");
+}
