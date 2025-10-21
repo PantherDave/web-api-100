3d896b50c3eecbcdaea5cbf13fd2b746430d9465
David Acosta <dacostacsit@gmail.com>
2025-10-21 10:06:00 -0400

    Add post

--- src/SoftwareCenterSolution/SoftwareCenterApi/Vendors/VendorsController.cs
+++ src/SoftwareCenterSolution/SoftwareCenterApi/Vendors/VendorsController.cs
@@ -9,4 +9,38 @@ public class VendorsController: ControllerBase
     {
         return Ok();
     }
+
+    [HttpPost("/vendors")]
+    public async Task<ActionResult> AddVendorAsync(
+        [FromBody] VendorCreateModel model
+        ) 
+    {
+        var response = new VendorDetailsModel
+        {
+            Id = Guid.NewGuid(),
+            Name = model.Name,
+            PointOfContact = model.PointOfContact,
+        };
+        return StatusCode(201, response);
+    }
+}
+
+public record VendorPointOfContact
+{ 
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
+
+public record VendorCreateModel 
+{
+    public string Name { get; set; } = string.Empty;
+    public VendorPointOfContact PointOfContact { get; set; } = new();
+}
+
+public record VendorDetailsModel 
+{
+    public Guid Id;
+    public string Name { get; set; } = string.Empty;
+    public VendorPointOfContact PointOfContact { get; set; } = new();
 }
