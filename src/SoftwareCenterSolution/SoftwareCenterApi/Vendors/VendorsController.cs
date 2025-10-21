eeecb34a65e476e9a5b43d96f3a22c71515472be
David Acosta <dacostacsit@gmail.com>
2025-10-21 14:57:42 -0400

    Add IDocumentSession to VendorsController

--- src/SoftwareCenterSolution/SoftwareCenterApi/Vendors/VendorsController.cs
+++ src/SoftwareCenterSolution/SoftwareCenterApi/Vendors/VendorsController.cs
@@ -1,8 +1,10 @@
-﻿namespace SoftwareCenterApi.Vendors;
+﻿using Marten;
+
+namespace SoftwareCenterApi.Vendors;
 
 // When we get a GET request to "/vendors", we want this controller to be created, and
 // a specific method on this controller to handle providing the response for the request
-public class VendorsController: ControllerBase
+public class VendorsController(IDocumentSession session): ControllerBase
 {
     [HttpGet("/vendors")]
     public async Task<ActionResult> GetAllVendorsAsync() 
@@ -15,6 +17,19 @@ public class VendorsController: ControllerBase
         [FromBody] VendorCreateModel model
         ) 
     {
+        // TODO: Validate the inputs, check auth all that stuff
+
+        //store the data somewhere
+
+        var entity = new VendorEntity
+        {
+            Id = Guid.NewGuid(),
+            Name = model.Name,
+            PointOfContact = model.PointOfContact,
+        };
+        session.Store(entity);
+        await session.SaveChangesAsync();
+
         var response = new VendorDetailsModel
         {
             Id = Guid.NewGuid(),
@@ -27,7 +42,25 @@ public class VendorsController: ControllerBase
     [HttpGet("/vendors/{id:Guid}")]
     public async Task<ActionResult> GetVendorByIdAsync(Guid id)
     {
-        return BadRequest();
+        var savedVendor = await session.Query<VendorEntity>()
+            .Where(v => v.Id == id)
+            .SingleOrDefaultAsync();
+
+        if (savedVendor == null)
+        {
+            return NotFound();
+        }
+        else 
+        {
+            var response = new VendorDetailsModel
+            {
+                Id = savedVendor.Id,
+                Name = savedVendor.Name,
+                PointOfContact = savedVendor.PointOfContact,
+            };
+
+            return Ok(response);
+        }
     }
 }
 
@@ -38,15 +71,26 @@ public record VendorPointOfContact
     public string Phone { get; set; } = string.Empty;
 }
 
+// 
 public record VendorCreateModel 
 {
     public string Name { get; set; } = string.Empty;
     public VendorPointOfContact PointOfContact { get; set; } = new();
 }
 
+// what I am returning to the caller on the POST and the GET /vendors/{id}
 public record VendorDetailsModel 
 {
-    public Guid Id;
+    public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public VendorPointOfContact PointOfContact { get; set; } = new();
+}
+
+// what I'm actually storing in the db
+public class VendorEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public VendorPointOfContact PointOfContact { get; set; } = new();
+    // who created this?
 }
